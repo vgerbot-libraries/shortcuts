0f7e695f1c725fceef0dfe1b1a3003185c0f4c04
y1j2x34 <y1j2x34@qq.com>
2021-11-24 23:22:16 +0800

    chore: jest configurations

--- packages/core/__tests__/src/index.spec.ts
+++ packages/core/__tests__/src/index.spec.ts
@@ -1,6 +1,6 @@
 import { hello } from '../../src/index';
 
-describe('test index.ts', ()=> {
+describe('test index.ts', () => {
     it('should hello method returns "world"', () => {
         expect(hello()).toBe('world');
     });
