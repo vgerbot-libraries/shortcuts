70c4a55c9d800bf17e178f31a42f21a219101bec
y1j2x34 <y1j2x34@qq.com>
2021-12-04 21:30:27 +0800

    feat: EventEmitter

---
+++ packages/core/src/event/EventEmitter.ts
@@ -0,0 +1,26 @@
+export type EventHandler<T> = (value: T) => void;
+
+export class EventEmitter<T> {
+    private events: Record<string, Array<EventHandler<T>>> = {};
+    on(name: string, handler: EventHandler<T>) {
+        const handlers = this.events[name] || [];
+        this.events[name] = handlers;
+        handlers.push(handler);
+        return () => {
+            const index = handlers.indexOf(handler);
+            if (index > 0) {
+                handlers.splice(index, 1);
+            }
+        };
+    }
+    emit(name: string, value: T) {
+        const handlers = this.events[name];
+        if (!handlers) {
+            return;
+        }
+        handlers.forEach(it => it(value));
+    }
+    clear() {
+        this.events = {};
+    }
+}
