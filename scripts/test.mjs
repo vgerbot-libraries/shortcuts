87f93a7ac77154778c7de2e9232fb1562cd52f91
y1j2x34 <y1j2x34@qq.com>
2021-12-21 22:34:26 +0800

    style: code style

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -2,4 +2,6 @@ import 'zx/globals';
 
 process.env.FORCE_COLOR = 3;
 console.log(`cd ${process.cwd()}`);
-await $`cross-env NODE_ENV=test jest -c ../../jest.config.js`.pipe(process.stdout)
+await $`cross-env NODE_ENV=test jest -c ../../jest.config.js`.pipe(
+    process.stdout
+);
