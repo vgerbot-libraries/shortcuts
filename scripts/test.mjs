b487cb71750ed06546773ca3de6c2ce36e5cffc3
y1j2x34 <y1j2x34@qq.com>
2022-01-15 22:04:52 +0800

    chore: supports to debug test code

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -1,9 +1,21 @@
 import { pkg } from './base.mjs';
 
-if (pkg.scripts && pkg.scripts.test) {
-    await $`cross-env NODE_ENV=test npm t`;
+if (process.env.ENV === 'DEBUG') {
+    if (pkg.scripts && pkg.scripts['test:debug']) {
+        await $`cross-env NODE_ENV=test npm test:debug`;
+    } else {
+        await $`cross-env NODE_ENV=test \
+        node --inspect-brk ../../node_modules/.bin/jest \
+        --runInBand -c ../../jest.config.js --passWithNoTests`.pipe(
+            process.stdout
+        );
+    }
 } else {
-    await $`cross-env NODE_ENV=test jest -c ../../jest.config.js --passWithNoTests`.pipe(
-        process.stdout
-    );
+    if (pkg.scripts && pkg.scripts.test) {
+        await $`cross-env NODE_ENV=test npm t`;
+    } else {
+        await $`cross-env NODE_ENV=test jest -c ../../jest.config.js --passWithNoTests`.pipe(
+            process.stdout
+        );
+    }
 }
