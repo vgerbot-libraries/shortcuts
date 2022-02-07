0323de8bc01b2b1670dc5757ef59db582532f809
y1j2x34 <y1j2x34@qq.com>
2022-02-07 22:46:33 +0800

    chore: update examples

--- scripts/eslint.mjs
+++ scripts/eslint.mjs
@@ -3,9 +3,9 @@ import 'zx/globals';
 process.env.FORCE_COLOR = 3;
 
 export async function runLint(argv = '') {
-    await $`eslint ${argv} --fix scripts/**/*.mjs`;
+    await $`eslint ${argv} scripts/**/*.mjs`;
 
     await $`lerna exec --scope=@shortcuts/angular-root -- eslint  ${argv} projects/**/*.ts`;
-
-    await $`lerna exec --ignore @shortcuts/angular-root -- eslint  ${argv} {src,__test__}/**/*.ts`;
+    // eslint-disable-next-line max-len
+    await $`lerna exec --ignore @shortcuts/angular-root --ignore @shortcuts/example-* -- eslint  ${argv} {src,__test__}/**/*.ts`;
 }
