0a00c0d65331bdb1ce5d34a06f61958d3a03b338
y1j2x34 <y1j2x34@qq.com>
2022-02-23 00:29:42 +0800

    test: EventEmitter

--- packages/core/__test__/src/event/EventEmitter.spec.ts
+++ packages/core/__test__/src/event/EventEmitter.spec.ts
@@ -2,25 +2,80 @@ import { EventEmitter } from '../../../src/event/EventEmitter';
 
 describe('event/EventEmitter', () => {
     describe('EventEmitter#emit', () => {
-        it('can emit the function with value', done => {
-            const emitter = new EventEmitter<string>();
-            emitter.on('foo', bar => {
-                expect(bar).toBe('bar');
-                done();
-            });
-            emitter.emit('foo', 'bar');
+        it('can emit the function with value', () => {
+            const e = new EventEmitter<string>();
+
+            const listener = jest.fn();
+
+            e.on('foo', listener);
+
+            e.emit('foo', 'bar');
+
+            expect(listener).toBeCalledWith('bar');
         });
-        it('can emit the function with multiple listeners', () => {
+        it('emits to all event listeners', () => {
             const e = new EventEmitter<void>();
-            const list: string[] = [];
-            e.on('foo', () => {
-                list.push('bar1');
-            });
-            e.on('foo', () => {
-                list.push('bar2');
-            });
+
+            const listener1 = jest.fn();
+            const listener2 = jest.fn();
+
+            e.on('foo', listener1);
+            e.on('foo', listener2);
+
             e.emit('foo');
-            expect(list).toEqual(['bar1', 'bar2']);
+
+            expect(listener1).toBeCalledTimes(1);
+            expect(listener2).toBeCalledTimes(1);
+        });
+        it('should return false when there are not events to emit', () => {
+            const e = new EventEmitter<void>();
+
+            const listener = jest.fn();
+
+            e.on('foo', listener);
+
+            expect(e.emit('foo')).toBeTruthy();
+            expect(e.emit('bar')).toBeFalsy();
+            expect(listener).toBeCalledTimes(1);
+        });
+    });
+    describe('EventEmitter#on', () => {
+        it('should remove only the listeners by the returning callback function', () => {
+            const e = new EventEmitter<void>();
+
+            const listener = jest.fn();
+
+            const removeListener = e.on('foo', listener);
+            e.on('foo', listener);
+
+            e.emit('foo');
+
+            removeListener();
+
+            e.emit('foo');
+
+            expect(listener).toBeCalledTimes(3);
+        });
+    });
+    describe('EventEmitter#clear', () => {
+        it('just clear all events', () => {
+            const e = new EventEmitter<void>();
+
+            const listener = jest.fn(() => {
+                throw new Error('oops');
+            });
+
+            e.on('foo', listener);
+            e.on('bar', listener);
+            e.on('baz', listener);
+
+            e.clear();
+
+            expect(e.emit('foo')).toBeFalsy();
+
+            expect(e.emit('bar')).toBeFalsy();
+
+            expect(e.emit('baz')).toBeFalsy();
         });
     });
 });
