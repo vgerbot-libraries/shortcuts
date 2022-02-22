6928c74c31f14b087eed79e5e1be5f22846ee25a
y1j2x34 <y1j2x34@qq.com>
2022-02-23 00:29:04 +0800

    refactor: implementation of EventEmitter

--- packages/core/src/event/EventEmitter.ts
+++ packages/core/src/event/EventEmitter.ts
@@ -1,27 +1,36 @@
 export type EventHandler<T> = (value: T) => void;
 
+interface EventPair<T> {
+    name: string;
+    handler: EventHandler<T>;
+}
+
 export class EventEmitter<T> {
-    private events: Record<string, Array<EventHandler<T>>> = {};
+    private events: Array<EventPair<T>> = [];
     on(name: string, handler: EventHandler<T>) {
-        const handlers = this.events[name] || [];
-        this.events[name] = handlers;
-        handlers.push(handler);
+        const pair = {
+            name,
+            handler
+        };
+        this.events.push(pair);
         return () => {
-            const index = handlers.indexOf(handler);
-            if (index > 0) {
-                handlers.splice(index, 1);
+            const index = this.events.indexOf(pair);
+            if (index > -1) {
+                this.events.splice(index, 1);
             }
         };
     }
     emit(name: string, value: T) {
-        const handlers = this.events[name];
-        if (!handlers) {
+        const handlers = this.events
+            .filter(it => it.name === name)
+            .map(it => it.handler);
+        if (handlers.length === 0) {
             return false;
         }
         handlers.forEach(it => it(value));
         return true;
     }
     clear() {
-        this.events = {};
+        this.events = [];
     }
 }
