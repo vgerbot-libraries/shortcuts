71154427e575291b8fb08be56f737b80b5609039
y1j2x34 <y1j2x34@qq.com>
2022-04-15 20:13:57 +0800

    style: sort-keys

--- packages/angular/projects/shortcuts/karma.conf.js
+++ packages/angular/projects/shortcuts/karma.conf.js
@@ -3,8 +3,30 @@
 
 module.exports = function (config) {
     config.set({
+        autoWatch: true,
         basePath: '',
+        browsers: ['ChromeHeadless'],
+        client: {
+            clearContext: false, // leave Jasmine Spec Runner output visible in browser
+            jasmine: {
+                // you can add configuration options for Jasmine here
+                // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+                // for example, you can disable the random execution with `random: false`
+                // or set a specific seed with `seed: 4321`
+            }
+        },
+        colors: true,
+        coverageReporter: {
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            dir: require('path').join(__dirname, '../../coverage/shortcuts'),
+            reporters: [{ type: 'lcov' }, { type: 'text-summary' }],
+            subdir: '.'
+        },
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        jasmineHtmlReporter: {
+            suppressAll: true // removes the duplicated traces
+        },
+        logLevel: config.LOG_INFO,
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
@@ -12,31 +34,9 @@ module.exports = function (config) {
             require('karma-coverage'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
-        client: {
-            jasmine: {
-                // you can add configuration options for Jasmine here
-                // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-                // for example, you can disable the random execution with `random: false`
-                // or set a specific seed with `seed: 4321`
-            },
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
-        },
-        jasmineHtmlReporter: {
-            suppressAll: true // removes the duplicated traces
-        },
-        coverageReporter: {
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            dir: require('path').join(__dirname, '../../coverage/shortcuts'),
-            subdir: '.',
-            reporters: [{ type: 'lcov' }, { type: 'text-summary' }]
-        },
-        reporters: ['progress', 'kjhtml'],
         port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['ChromeHeadless'],
-        singleRun: true,
-        restartOnFileChange: true
+        reporters: ['progress', 'kjhtml'],
+        restartOnFileChange: true,
+        singleRun: true
     });
 };
